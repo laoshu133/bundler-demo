737acf6907e5c5edac9b18d38cbd117f1f0e84e2
laoshu133 <admin@laoshu133.com>
2016-11-23 01:11:25 +0800

    Add rollup demo

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+export default {
+    entry: 'src/app.js',
+    plugins: [
+        // babel({
+        //     runtimeHelpers: true,
+        //     exclude: 'node_modules/**',
+        //     presets: ['es2015-rollup']
+        // }),
+        nodeResolve({ jsnext: true }),
+        commonjs()
+    ],
+    targets: [ {
+        format: 'umd',
+        dest: 'dist/rollup/app.js'
+    } ]
+};
--- src/foo.js
+++ src/foo.js
@@ -4,6 +4,10 @@
 
 import $ from 'jquery';
 
+const bar = function() {
+    console.log('Unused function');
+};
+
 const foo = {
     baz() {
         console.log('Links length: ' + $('a[href]').length);
